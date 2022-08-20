294e6cba78c58f0cb136d1a2ecc4cadc23287e5f
MostValue <kerui.yang@outlook.com>
2022-08-21 09:13:56 +1000

    Added subclass Limiting Reagent and its method .create()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, randint
 from chemlib import *
 
 class Question:
@@ -12,7 +12,7 @@ class Question:
     def create(self):
         pass
 
-class Mass(Question):
+class MassPercent(Question):
     compounds = ["H2O", "H2SO4", "Na2SO3", "Na2PO4"]
 
     def __init__(self,question,answer):
@@ -27,8 +27,37 @@ class Mass(Question):
         self.compounds.remove(x)
 
 
+class LimitingReagent(Question):
+    reactions = [
+        "N2O5 + H2O --> HNO3",
+        "CO2 + H2O --> O2 + C6H12O6",
+        "H2 + O2 --> H2O",
+        "Ca + CO3 --> CaCO3",
+        "H2 + N2 --> NH3",
+        "CH3CH3OH + O2 --> CH3COOH + H2O",
+        "CH4 + O2 --> H2O + CO2",
+    ]
 
-q = Mass("Question","answer")
+    def __init__(self,question,answer):
+        super().__init__(question, answer)
+
+    def create(self):
+        n = choice(self.reactions)
+        print(n)
+        r = Reaction.by_formula(n)
+        print(r)
+        r.balance()
+        first = r.reactant_formulas[1]
+        second = r.reactant_formulas[2]
+        n1 = randint(1,100)
+        n2 = randint(1,100)
+        limiting = r.limiting_reagent(n1, n2)
+        self.question = f"What is the limiting reagent in the reaction: {r.formula} \n Given I have {n1} grams of {first} and {n2} grams of {second}?"
+        self.answer = limiting.formula
+        self.reactions.remove(n)
+
+
+q = LimitingReagent("Question","answer")
 q.create()
 print(q.check(input(q.question)))
 print(q.answer)
