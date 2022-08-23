6f9a57c23c27508e3fcdfc53b3c3ae30460f97e9
MostValue <kerui.yang@outlook.com>
2022-08-23 18:14:27 +1000

    Added subclass Flowchart and methods create() and check()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import choice, randint
 from chemlib import *
 
+
 class Question:
     def __init__(self, question, answer):
         self.question = question
@@ -34,7 +35,7 @@ class LimitingReagent(Question):
         "H2 + O2 --> H2O",
         "Ca + CO3 --> CaCO3",
         "H2 + N2 --> NH3",
-        "CH3CH3OH + O2 --> CH3COOH + H2O",
+        "CH3CH3OH + O2 --> CH3CO2H + H2O",
         "CH4 + O2 --> H2O + CO2",
     ]
 
@@ -47,8 +48,9 @@ class LimitingReagent(Question):
         r = Reaction.by_formula(n)
         print(r)
         r.balance()
-        first = r.reactant_formulas[1]
-        second = r.reactant_formulas[2]
+        first = r.reactant_formulas[0]
+        second = r.reactant_formulas[-1]
+        print(r.reactant_formulas)
         n1 = randint(1,100)
         n2 = randint(1,100)
         limiting = r.limiting_reagent(n1, n2)
@@ -56,8 +58,62 @@ class LimitingReagent(Question):
         self.answer = limiting.formula
         self.reactions.remove(n)
 
+class Flowchart(Question):
+    #HCl = {"Descriptor": "Ppt", "Pb2+": "White ppt", "Ag+": "White ppt"}
+    #Excess_NH3 = {"Ag+": "Ppt dissolves", "Pb2+": "Ppt doesnt dissolve"}
+    #H2SO4 = {"Descriptor": "Ppt", "Ba2+": "White ppt", "Ca2+": "White ppt"}
+    #NaOH = {"None": "No Ppt", "Mg2+": "White ppt", "Cu2+": "Blue ppt", "Fe2+": "Green Ppt", "Fe3+":"Brown ppt"}
+    Tests = ["HCl" , "Excess_NH3","H2SO4","NaOH", "Flame"]
+    Cations = ['Pb2+', 'Ag+', 'Ca2+', 'Ba2+', 'Mg2+', 'Fe2+', 'Fe3+', 'Cu2+']
+    Pb2 = {'Identity': 'Pb2+','HCl' : True , 'H2SO4' : True, 'NaOH':True, 'Excess_NH3': True,"Flame":"N/A"}
+    Ag =  {'Identity': 'Ag+','HCl':True, 'H2SO4':True,'NaOH':True, 'Excess_NH3': False}
+    Ca2 = {'Identity': 'Ca2+','HCl':False, 'H2SO4':True,'NaOH': False, 'Excess_NH3': False,"Flame":"Brick Red"}
+    Ba2 = {'Identity': 'Ba2+','HCl':False, 'H2SO4':True,'NaOH': False, 'Excess_NH3': False, "Flame":"Apple Green"}
+    Mg2 = {'Identity': 'Mg2+','HCl':False, 'H2SO4':False,'NaOH': "White ppt", 'Excess_NH3': "White ppt","Flame":"White"}
+    Fe2 = {'Identity': 'Fe2+','HCl':False, 'H2SO4':False,'NaOH': "Green ppt", 'Excess_NH3': "Green ppt", "Flame":"N/A"}
+    Fe3 = {'Identity': 'Fe3+','HCl':False, 'H2SO4':False,'NaOH': "Brown ppt", 'Excess_NH3': "Brown ppt","Flame":"N/A"}
+    Cu2 = {'Identity': 'Cu2+','HCl':False, 'H2SO4':False,'NaOH': "Blue ppt", 'Excess_NH3': "Blue ppt","Flame":"N/A"}
+    Properties = [Pb2,Ag,Ca2,Ba2,Mg2,Fe3,Fe2,Cu2]
+    cation = None
 
-q = LimitingReagent("Question","answer")
+    def __init__(self,question,answer):
+        super().__init__(question, answer)
+    def create(self):
+        self.cation = choice(self.Properties)
+        self.question = f"Use a series of tests to identify the unknown cation.\n Possible cations: {self.Cations}\n Possible tests:\n"
+        for i in range(len(self.Tests)):
+           self.question += f"({i+1}) {self.Tests[i]}\n"
+        self.answer = self.cation['Identity']
+        print(self.answer)
+        self.Properties.remove(self.cation)
+    def check(self):
+        limit = 4
+        print(self.question)
+        for i in range(limit):
+            response = input(f'Guesses Remaining ({limit-i}): ')
+            if response.isdigit() and int(response) > 0 and int(response) < len(self.Tests)+1:
+                answer = self.Tests[int(response)-1]
+                if self.cation[answer] == False:
+                    print("No ppt")
+                elif self.cation[answer] == True:
+                    print("White ppt")
+                else:
+                    print(self.cation[answer])
+            elif response == self.answer:
+                print("Correct")
+                break
+            else:
+                print("Invalid test and/or incorrect answer")
+
+        #while response in self.cation:
+            #print (self.cation[response])
+            #response = input(self.question)
+            #if response == str(self.answer):
+                #print("Correct")
+            #else:
+                #print("Invalid test and/or incorrect answer")
+
+q = Flowchart("Question","Answer")
 q.create()
-print(q.check(input(q.question)))
-print(q.answer)
+q.check()
+#while q.check(input(q.question)) != "Correct":
